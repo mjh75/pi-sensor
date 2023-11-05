8d19615853d1fa87ae95e0e7b0205676c496dfb7
Michael Hartwick <hartwick@hartwick.com>
2023-11-04 22:00:17 -0400

    Update to python3 and replace the Adafruit_DHT library with adafruit_dht

--- ultrasonic_distance.py
+++ ultrasonic_distance.py
@@ -1,55 +1,110 @@
-#!/usr/bin/env python
-
+#!/usr/bin/env python3
+import atexit
 #Libraries
-import RPi.GPIO as GPIO
-import Adafruit_DHT
-from gpiozero import CPUTemperature
 import time
 import math
- 
-#GPIO Mode (BOARD / BCM)
-GPIO.setmode(GPIO.BCM)
- 
+import logging
+from RPi import GPIO
+from gpiozero import DistanceSensor
+from pulseio import PulseIn
+import board
+import adafruit_dht
+from gpiozero import CPUTemperature
+
+
 #set GPIO Pins
 GPIO_TRIGGER = 18
 GPIO_ECHO = 24
- 
-#set GPIO direction (IN / OUT)
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
- 
-def distance():
+
+
+
+def get_environment(dht):
+    """
+    Read the humidity and temperature from the
+    DHT11
+    :return:
+    """
+    retval = {}
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    try:
+        retval['humidity'] = dht.humidity
+        logging.debug(f"Measured: {retval['humidity']}%")
+    except RuntimeError as error:
+        retval['humidity'] = 44
+        logging.debug(f"Static: {retval['humidity']}%")
+    try:
+        retval['temperature'] = dht.temperature
+        logging.debug(f"Measured: {retval['temperature']}C")
+    except RuntimeError as error:
+        retval['temperature'] = 25
+        logging.debug(f"Static: {retval['temperature']}C")
+    return retval
+
+
+def determine_speed_of_sound(temperature):
+    """
+    Calculate the speed of sound based on the current temperature
+    :param temperature:
+    :return:
+    """
+    speed_of_sound = 331.3 * math.sqrt(1 + (temperature / 273.15))
+    logging.debug(f"Calculated speed of sound: {speed_of_sound} m/s")
+    return speed_of_sound
+
+def distance(dht):
+    """
+    Measure the distance
+    :return: retval dictionary with measured or calculated values
+    """
+    retval = {}
     # set Trigger to HIGH
     GPIO.output(GPIO_TRIGGER, True)
- 
+
     # set Trigger after 0.01ms to LOW
     time.sleep(0.00001)
     GPIO.output(GPIO_TRIGGER, False)
- 
-    StartTime = time.time()
-    StopTime = time.time()
- 
-    # save StartTime
+
+    # save start_time
     while GPIO.input(GPIO_ECHO) == 0:
-        StartTime = time.time()
- 
+        start_time = time.perf_counter_ns()
+
     # save time of arrival
     while GPIO.input(GPIO_ECHO) == 1:
-        StopTime = time.time()
- 
+        stop_time = time.perf_counter_ns()
+
     # time difference between start and arrival
-    TimeElapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-    speed_of_sound = 331 * math.sqrt ( 1 + (temperature / 273))
-    distance = (TimeElapsed * speed_of_sound) / 2
-    cputemp = CPUTemperature()
- 
-    return(distance, temperature, humidity, cputemp.temperature)
- 
-if __name__ == '__main__':
-    dist, temperature, humidity, cputemp = distance()
-    print ('Distance: {0:0.2f} m Temp: {1:0.5f} C  Humidity: {2:0.5f} %  CPU: {3:0.5f} C'.format(dist, temperature, humidity, cputemp))
-    time.sleep(1)
+    time_elapsed = stop_time - start_time
+
+    environment = get_environment(dht)
+    retval['humidity'] = environment['humidity']
+    retval['temperature'] = environment['temperature']
+    retval['speed_of_sound'] = determine_speed_of_sound(retval['temperature'])
+    retval['distance'] = (time_elapsed / 1000000000 * retval['speed_of_sound']) / 2
+
+    return retval
+
+
+def cleanup():
+    """
+    Cleanup on exit
+    :return:
+    """
     GPIO.cleanup()
+    logging.debug("Exiting")
+
+if __name__ == '__main__':
+    atexit.register(cleanup)
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+    # set GPIO direction (IN / OUT)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+    GPIO.setup(GPIO_ECHO, GPIO.IN)
+
+    # Configure the DHT sensor
+    dhtDevice = adafruit_dht.DHT11(board.D4, use_pulseio=True)
+    while True:
+        data = distance(dhtDevice)
+        cputemp = CPUTemperature().temperature
+        print(f"Distance: {data['distance']:0.10f} m Temp: {data['temperature']:0.5f} C  Humidity: {data['humidity']:0.5f} %  CPU: {cputemp} C")
+        time.sleep(1)
